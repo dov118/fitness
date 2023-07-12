886b8472b73aa1b00670f9d38716f1bf80d3d6c6
dov118 <contact@dov118.dev>
2023-07-12 21:30:45 +0200

    code review
    Took 7 minutes

--- src/database/migrations/2023_07_02_081014_create_equipment_table.php
+++ src/database/migrations/2023_07_02_081014_create_equipment_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('equipment', function (Blueprint $table) { // TODO à changer en equipments
+        Schema::create('equipments', function (Blueprint $table) {
             $table->id();
 
             $table->string('name', 64)
@@ -66,6 +66,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('equipment');
+        Schema::dropIfExists('equipments');
     }
 };

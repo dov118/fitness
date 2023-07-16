d50e8fa7a89b96b7aebdb4c3510a7f3ef2015e01
dov118 <contact@dov118.dev>
2023-07-16 12:39:59 +0200

    code review
    Took 9 minutes

--- src/tests/Browser/Pages/Page.php
+++ src/tests/Browser/Pages/Page.php
@@ -14,7 +14,20 @@ abstract class Page extends BasePage
     public static function siteElements(): array
     {
         return [
-            '@element' => '#selector',
+            '@header-link-login' => '.Header-link--login',
+            '@user-menu-avatar' => '.user-menu-avatar',
+            '@user-menu-avatar-img' => '.user-menu-avatar img',
+            '@user-menu-email' => '.user-menu-email',
+            '@user-menu-dashboard' => '.user-menu-dashboard',
+            '@user-menu-logout' => '.user-menu-logout',
+            '@side-nav-general-group-title' => '.sidenav .menu-heading--general',
+            '@side-nav-dashboard-link' => '.sidenav .menu-item--dashboard',
+            '@side-nav-muscle-link' => '.sidenav .menu-item--muscle',
+            '@side-nav-muscle-link-counter' => '.sidenav .menu-item--muscle-count',
+            '@side-nav-group-link' => '.sidenav .menu-item--group',
+            '@side-nav-group-link-counter' => '.sidenav .menu-item--group-count',
+            '@side-nav-exercise-link' => '.sidenav .menu-item--exercise',
+            '@side-nav-exercise-link-counter' => '.sidenav .menu-item--exercise-count',
         ];
     }
 }

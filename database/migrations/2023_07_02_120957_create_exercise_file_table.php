350046fc690f5cbbba2638dd907d15154dbc3f74
dov118 <contact@dov118.dev>
2023-07-17 18:10:21 +0200

    code review
    Took 5 minutes

--- database/migrations/2023_07_02_120957_create_exercise_file_table.php
+++ database/migrations/2023_07_02_120957_create_exercise_file_table.php
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('exercise_files');
+        Schema::dropIfExists('exercise_file');
     }
 };

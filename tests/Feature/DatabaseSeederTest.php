f986297df7e7ad2cef51a6dbd6b0ede6f82ea0cc
dov118 <contact@dov118.dev>
2024-01-29 10:23:20 +0100

    Update
    Took 43 minutes

--- tests/Feature/DatabaseSeederTest.php
+++ tests/Feature/DatabaseSeederTest.php
@@ -3,12 +3,10 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class DatabaseSeederTest extends TestCase
 {
-    use RefreshDatabase;
     use DatabaseMigrations;
 
     /**
